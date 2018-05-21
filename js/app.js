ed9f2aea8658d4e8c1c1f1eae39acc8fd3b89b06
Jeon <jcawaling2@gmail.com>
2018-05-21 15:02:14 -0700

    add sticky

--- js/app.js
+++ js/app.js
@@ -1,18 +1,24 @@
-const nav = document.querySelector('#navigation');
-const navTop = nav.offsetTop;
+// var waypoint = require('/waypoints/lib/jquery.waypoints.min.js');
 
-function stickyNavigation() {
-  console.log('navTop = ' + navTop);
-  console.log('scrollY = ' + window.scrollY);
+$(document).ready(function(){
+	$('.js--section-about').waypoint(function(direction){
+		if(direction == "down"){
+			$('nav').addClass('sticky');
+			$('span:first-child').removeClass('name').addClass('name2');
+;
+		}
+		else {
+			$('nav').removeClass('sticky');
+			$('span:first-child').removeClass('name2').addClass('name');
+		}
+	}, {
+		offset: '100px'
+	});
 
-  if (window.scrollY >= navTop) {
-    // nav offsetHeight = height of nav
-    document.body.style.paddingTop = nav.offsetHeight + 'px';
-    document.body.classList.add('fixed-nav');
-  } else {
-    document.body.style.paddingTop = 0;
-    document.body.classList.remove('fixed-nav');
-  }
-}
+	// var waypoints = $('#handler-first').waypoint(function(direction) {
+	//   notify(this.element.id + ' hit 25% from top of window')
+	// }, {
+	//   offset: '25%'
+	// })
 
-window.addEventListener('scroll', stickyNavigation);
+});
